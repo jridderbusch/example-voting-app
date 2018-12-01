a637e1569fed3083bdca377c6063db0ba7a74afb
janoler <jan.ridderbusch@tu-dortmund.de>
2018-12-01 15:36:11 +0100

    Migrated view of result service to vue.js

--- result/views/app.js
+++ result/views/app.js
@@ -1,50 +1,91 @@
-var app = angular.module('catsvsdogs', []);
-var socket = io.connect({transports:['polling']});
+var socket = io.connect({
+  transports: ["polling"]
+})
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
+Vue.use(VueCharts)
+var result = new Vue({
+  el: "#app",
+  data: {
+    chartBind: true,
+    chartWidth: 600,
+    chartHeight: 600,
+    chartScale: false,
+    chartLabels: ["Cats", "Dogs"],
+    chartData: [],
+    chartBackgroundColor: [
+      "rgba(33, 150, 243, 0.3)",
+      "rgba(0, 203, 202, 0.3)",
+      "rgba(9, 205, 81, 0.3)"
+    ],
+    chartHoverBackgroundColor: [
+      "rgba(33, 150, 243, 1)",
+      "rgba(0, 203, 202, 1)",
+      "rgba(9, 205, 81, 1)"
+    ],
+    chartBorderColor: [
+      "rgba(33, 150, 243, 1)",
+      "rgba(0, 203, 202, 1)",
+      "rgba(9, 205, 81, 1)"
+    ],
+    countString: ""
+  },
+  methods: {
+    updateLabels(newLabels) {
+        if (!compareArrays(newLabels, this.chartLabels))
+          this.chartLabels = newLabels
+      },
+      updateData(newData) {
+        if (!compareArrays(newData, this.chartData))
+          this.chartData = newData
+      },
+      updateCount(newCount) {
+        if (newCount == 0)
+          this.countString = "Keine Stimmen abgegeben."
+        else if (newCount == 1)
+          this.countString = "Eine Stimme abgegeben."
+        else
+          this.countString = newCount + " Stimmen abgegeben."
+      }
+  }
+})
 
-app.controller('statsCtrl', function($scope){
-  $scope.aPercent = 50;
-  $scope.bPercent = 50;
+var updateScores = () => {
+  socket.on('scores', function(json) {
+    data = JSON.parse(json)
+    var a = parseInt(data.a || 0)
+    var b = parseInt(data.b || 0)
 
-  var updateScores = function(){
-    socket.on('scores', function (json) {
-       data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+    var percentages = getPercentages(a, b)
 
-       var percentages = getPercentages(a, b);
+    result.updateData([percentages.a, percentages.b])
+    result.updateCount(a + b)
+  })
+}
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
-
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
-    });
-  };
-
-  var init = function(){
-    document.body.style.opacity=1;
-    updateScores();
-  };
-  socket.on('message',function(data){
-    init();
-  });
-});
+socket.on('message', function(data) {
+  updateScores()
+})
 
 function getPercentages(a, b) {
-  var result = {};
+  var result = {}
 
   if (a + b > 0) {
-    result.a = Math.round(a / (a + b) * 100);
-    result.b = 100 - result.a;
+    result.a = Math.round(a / (a + b) * 100)
+    result.b = 100 - result.a
   } else {
-    result.a = result.b = 50;
+    result.a = result.b = 50
   }
 
-  return result;
+  return result
+}
+
+function compareArrays(a, b) {
+  if (a.length != b.length)
+    return false
+
+  for (var i = 0; i < a.length; i++)
+    if (a[i] != b[i])
+      return false
+
+  return true
 }
