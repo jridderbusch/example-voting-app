dc8684cc68075d9cf4173a6a5cfd280c7761cb6e
tillepille <16536696+tillepille@users.noreply.github.com>
2018-12-06 09:21:16 +0100

    add own view

--- result/server.js
+++ result/server.js
@@ -1,86 +1,75 @@
-var express = require('express'),
-    async = require('async'),
-    pg = require("pg"),
-    path = require("path"),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+const express = require('express'),
+	async = require('async'),
+		pg = require("pg"),
+		path = require("path"),
+		cookieParser = require('cookie-parser'),
+		bodyParser = require('body-parser'),
+		methodOverride = require('method-override'),
+		app = express(),
+		server = require('http').Server(app);
 
-io.set('transports', ['polling']);
+const port = process.env.PORT || 4000;
+const options  = process.env.OPTIONS || "Cats,Dogs"
+options = options.split(',')
+var votes= {}
 
-var port = process.env.PORT || 4000;
 
-io.sockets.on('connection', function (socket) {
-
-  socket.emit('message', { text : 'Welcome!' });
-
-  socket.on('subscribe', function (data) {
-    socket.join(data.channel);
-  });
-});
-
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pg.connect('postgres://postgres@db/postgres', function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
-    });
-  },
-  function(err, client) {
-    if (err) {
-      return console.error("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
-  }
+async.retry({times: 1000,interval: 1000}, (callback) => {
+		pg.connect('postgres://postgres@db/postgres', (err, client, done) => {
+			if (err) {
+				console.error("Waiting for db");
+			}
+			callback(err, client);
+		});
+	},
+	(err, client) => {
+		if (err) {
+			return console.error("Giving up");
+		}
+		console.log("Connected to db");
+		getVotes(client);
+	}
 );
 
 function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
-
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
+	client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], (err, result) => {
+		if (err) {
+			console.error("Error performing query: " + err);
+		} else {
+			result.rows.forEach((row) => votes[row.vote] = parseInt(row.count))
+      console.log(votes);
+		}
+		setTimeout(() => {
+			getVotes(client)
+		}, 3000);
+	});
 }
 
-function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0, c: 0};
-
-  result.rows.forEach(function (row) {
-    votes[row.vote] = parseInt(row.count);
-  });
-
-  return votes;
-}
 
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+	next();
 });
 
 app.use(express.static(__dirname + '/views'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
-server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
+app.get('/result', (req, res) => {
+	res.json(votes)
+})
+app.get('/options', (req, res) => {
+  res.json(options)
+})
+
+server.listen(port, () => {
+	var port = server.address().port;
+	console.log('App running on port ' + port);
 });
--- result/views/app.js
+++ result/views/app.js
@@ -1,120 +1,68 @@
-var socket = io.connect({
-  transports: ["polling"]
-})
+google.charts.load('current', {
+	'packages': ['corechart']
+});
 
-Vue.use(VueCharts)
-var result = new Vue({
-  el: "#app",
-  data: {
-    chartBind: true,
-    chartWidth: 600,
-    chartHeight: 600,
-    chartScale: false,
-    chartLabels: [],
-    chartData: [],
-    chartBackgroundColor: [
-      "rgba(33, 150, 243, 0.3)",
-      "rgba(0, 203, 202, 0.3)",
-      "rgba(9, 205, 81, 0.3)"
-    ],
-    chartHoverBackgroundColor: [
-      "rgba(33, 150, 243, 1)",
-      "rgba(0, 203, 202, 1)",
-      "rgba(9, 205, 81, 1)"
-    ],
-    chartBorderColor: [
-      "rgba(33, 150, 243, 1)",
-      "rgba(0, 203, 202, 1)",
-      "rgba(9, 205, 81, 1)"
-    ],
-    countString: ""
-  },
-  methods: {
-    updateLabels(newLabels) {
-        if (!compareArrays(newLabels, this.chartLabels))
-          this.chartLabels = newLabels
-      },
-      updateData(newData) {
-        if (!compareArrays(newData, this.chartData))
-          this.chartData = newData
-      },
-      updateCount(newCount) {
-        if (newCount == 0)
-          this.countString = "Keine Stimmen abgegeben."
-        else if (newCount == 1)
-          this.countString = "Eine Stimme abgegeben."
-        else
-          this.countString = newCount + " Stimmen abgegeben."
-      }
-  }
-})
+var app = new Vue({
+	el: '#app',
+	data: {
+		options: [],
+		votes: {},
+		dataTable: [],
+	},
+	methods: {
+		create: function () {
+			fetch('/options', {
+					method: 'GET'
+				})
+				.then(res => {
+					return res.json()
+				})
+				.then(options => {
+					this.options = options
+					this.update()
+				})
+				.catch(e => console.log(e))
+		},
+		update: function () {
+			fetch('/result', {
+					method: 'GET'
+				})
+				.then(res => {
+					return res.json()
+				})
+				.then(votes => {
+					this.votes = votes
+					this.dataTable = this.buildData()
+					drawVotes(this.dataTable)
+					setTimeout(() => this.update(), 1000)
+				})
+				.catch(e => console.log(e))
+		},
+		buildData: function () {
+			var data = [
+				['Votes', 'Percent']
+			]
+			var voteKeys = Object.keys(this.votes).sort()
+			for (i = 0; i < this.options.length; i++) {
+				data.push([this.options[i], this.votes[voteKeys[i]]])
+			}
+			return data
+		}
 
-loadJSON("appsettings.json", (object) => {
-  result.updateLabels(object.options)
-})
+	},
+});
 
-var updateScores = () => {
-  socket.on('scores', function(json) {
-    data = JSON.parse(json)
-    var a = parseInt(data.a || 0)
-    var b = parseInt(data.b || 0)
-    var c = parseInt(data.c || 0)
+google.charts.setOnLoadCallback(app.create);
 
-    var percentagesAndSum = getPercentages([a, b, c])
-
-    result.updateData(percentagesAndSum.percentages)
-    result.updateCount(percentagesAndSum.sum)
-  })
-}
-
-socket.on('message', function(data) {
-  updateScores()
-})
-
-function getPercentages(array) {
-  var result = {
-    "percentages": [],
-    "sum": 0
-  }
-
-  if (array.length > 0) {
-    var sum = array.reduce((a, b ) => a + b, 0)
-    result.sum = sum
-
-    for (var i = 0; i < array.length; i++)
-      result.percentages.push((array[i] * 100 / sum).toFixed(2))
-  } else {
-    result.percentages = [50, 50]
-  }
-
-  return result
-}
-
-function compareArrays(a, b) {
-  if (a.length != b.length)
-    return false
-
-  for (var i = 0; i < a.length; i++)
-    if (a[i] != b[i])
-      return false
-
-  return true
-}
-
-function loadJSON(filePath, success, error) {
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        if (success)
-          success(JSON.parse(xhr.responseText))
-      }
-      else {
-        if (error)
-          error(xhr)
-      }
-    }
-  }
-  xhr.open("GET", filePath, true)
-  xhr.send()
+function drawVotes(table) {
+	var data = google.visualization.arrayToDataTable(table);
+	var options = {
+		colors: ['#2196f3', '#00cbca'],
+		legend: {
+			position: 'top',
+			alignment: 'center'
+		}
+	};
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	chart.draw(data, options);
 }
